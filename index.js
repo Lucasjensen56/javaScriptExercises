36552548f6b13c5a3d557d42b74a7da5724e276a
Lucas Jensen <lucasjensen@er-10-132-30-92.wireless.umn.edu>
2018-02-24 11:44:06 -0600

    more javaScript testing

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-earl
+
 'use strict';
 
-const sentence = 'this is a string';
-
+const name = 'Josh';
 let happy = true;
+let age = 33;
+const hairColor = 'red';
+const test = 1/0;
 
-console.log(typeof sentence, sentence);
-console.log(typeof happy, happy);
+// const joshArray = [name, happy, age, hairColor];
+
+const josh = [name, happy, age, hairColor, ['kids', 'dogs', 4]];
+
+
+if (josh[1]===true) {
+    console.log(Array.isArray(josh), josh[4][0]);
+
+}
+
+
+
+
+// console.log(Array.isArray(joshArray), joshArray[4][0]);
+
+// console.log(typeof joshArray, joshArray);
+
+
+//  console.log(Array.isArray(joshArray), joshArray[2][1]);
+
+
+// // console.log(typeof sentence, sentence);
+// // console.log(typeof happy, happy);
+// // happy = false;
+// // console.log(typeof happy, happy);
+// // console.log(typeof age, age);
+// console.log(typeof hairColor, hairColor)
