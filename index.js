f2e0b13aa4ee071e593bdfd7b1fd8dc36c928b23
Lucas Jensen <lucasjensen@er-10-132-30-92.wireless.umn.edu>
2018-02-24 13:09:03 -0600

    testing out for loops

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
 
 'use strict';
 
-const name = 'Josh';
-let happy = true;
-let age = 33;
-const hairColor = 'red';
-const test = 1/0;
 
-// const joshArray = [name, happy, age, hairColor];
-
-const josh = [name, happy, age, hairColor, ['kids', 'dogs', 4]];
-
-
-if (josh[1]===true) {
-    console.log(Array.isArray(josh), josh[4][0]);
-
+const josh = {
+    name: 'Josh', 
+    age: 33,
+    happy: true,
+    hairColor: 'red',
 }
+
+const peter = {
+    name: 'Peter',
+    age: 27,
+    happy: true,
+    hairColor: 'brown',
+}
+
+
+
+const studentArray = [josh, peter];
+
+
+
+
+
+// console.log(studentArray);
+
+// only showing the name property /dollar sign curly braces is how you insert a variable 
+for (let i = 0; i < studentArray.length; i++) {
+    console.log(`Hello, ${studentArray[i].name}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const name = 'Josh';
+// let happy = true;
+// let age = 33;
+// const hairColor = 'red';
+// const test = 1/0;
+
+// // const joshArray = [name, happy, age, hairColor];
+
+// const josh = [name, happy, age, hairColor, ['kids', 'dogs', 4]];
+
+
+// if (josh[1]===true) {
+//     console.log(Array.isArray(josh), josh[4][0]);
+// }
+
+
+
+// for (let i =0; i < josh.length; i++) {
+//     console.log(josh[i]);
+// }
+
+
+
+
+
+
 
 
 
